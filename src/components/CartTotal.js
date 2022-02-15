bf974e5b2d711f6c31165ab2212c60a0f910df6b
MarcoJul <82112168+MarcoJul@users.noreply.github.com>
2022-02-15 23:07:24 +0100

    feat: add reducers to <CartTotal/>

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,21 +1,26 @@
 import { Fragment } from "react";
+import { useSelector } from "react-redux";
 import classes from "./CartTotal.module.css";
 
 const CartTotal = () => {
+  const subTotal = useSelector((state) => state.subTotal);
+  const taxes = useSelector((state) => state.taxes);
+  const totalCart = useSelector((state) => state.totalCart);
+
   return (
     <Fragment>
       <div className={classes.box}>
         <div className={classes.row}>
           <h3>Subtotal:</h3>
-          <p className={classes.price}>$20.10</p>
+          <p className={classes.price}>${subTotal}</p>
         </div>
         <div className={classes.row}>
           <h3>Tax:</h3>
-          <p className={classes.taxes}>$1.96</p>
+          <p className={classes.taxes}>${taxes}</p>
         </div>
         <div className={classes.row}>
           <h3>Total:</h3>
-          <p className={classes.total}>$30.12</p>
+          <p className={classes.total}>${totalCart}</p>
         </div>
       </div>
     </Fragment>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
 
-const cartReducer = (state, action) => {
+const cartReducer = (
+  state = { subTotal: 0, taxes: 0, totalCart: 0 },
+  action
+) => {
   return state;
 };
 
