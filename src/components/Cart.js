10843276575e4028889d92791a71c218c28f61fd
MarcoJul <82112168+MarcoJul@users.noreply.github.com>
2022-02-15 23:38:05 +0100

    feat: change Cart visibility

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
+
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
 import CartTotal from "./CartTotal";
 
 const Cart = () => {
+  const [isEmpty, setIsEmpty] = useState(true);
   return (
     <div className={classes.box}>
       <h2 className={classes.title}>Your Cart</h2>
-      <p className={classes.empty}>Your Cart is empty.</p>
-      <CartItem />
+      {!isEmpty && <p className={classes.empty}>Your Cart is empty.</p>}
+      {!isEmpty && <CartItem />}
       <CartTotal />
     </div>
   );
